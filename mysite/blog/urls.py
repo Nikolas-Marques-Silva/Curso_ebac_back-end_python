6953a17bf2c1d2f510b5300c49591f8e54f8164b
Nikolas-Marques-Silva <nikolas.marques.da.silva1@gmail.com>
2024-12-10 18:13:20 -0300

    :sparkles feat: Django Template
    Nesse módulo aprendemos a utilizar a camada de template, onde brincamos
    um pouco com a parte front-end do projeto

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from blog import views
+from blog.views.post_view import PostDetail
 
 urlpatterns = [
-  path('', views.PostView.as_view(), name='home')
+  path('', views.PostView.as_view(), name='home'),
+  path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail")
 ]
