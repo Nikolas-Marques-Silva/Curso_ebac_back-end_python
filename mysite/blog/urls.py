c066289d4ac2dfb0b607d001a96616793d9b44ac
Níkolas Marques da Silva <nikolas.marques.da.silva1@gmail.com>
2024-12-27 13:56:25 -0300

    :sparkles feat: Diagramas Rest Api Django

--- mysite/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from blog import views
-from blog.views.post_view import PostDetail
-
-urlpatterns = [
-  path('', views.PostView.as_view(), name='home'),
-  path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail")
-]
